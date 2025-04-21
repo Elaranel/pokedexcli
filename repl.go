919823442749f0deccf3321676a508891e1749bf
Elaranel <elaranel@gmail.com>
2025-04-21 15:49:29 +0100

    Setup basic command prompt style interface

--- repl.go
+++ repl.go
@@ -1,6 +1,28 @@
 package main
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func startRepl() {
+	reader := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		reader.Scan()
+
+		input := cleanInput(reader.Text())
+		if len(input) == 0 {
+			continue
+		}
+
+		commandName := input[0]
+
+		fmt.Printf("Your command was: %s\n", commandName)
+	}
+}
 
 func cleanInput(text string) []string {
 
